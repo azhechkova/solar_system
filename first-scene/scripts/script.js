743f77d216a5f06edb5af060a6cedc6fcd9b1bfb
alina_zhechkova <alina_zhechkova@pecodesoftware.com>
2022-06-18 14:55:23 +0300

    First basic scene: rotating cube

---
+++ first-scene/scripts/script.js
@@ -0,0 +1,36 @@
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "#1a3a4c" });
+
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const camera = new THREE.PerspectiveCamera(55, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.x = 1;
+
+scene.add(camera);
+
+const canvas = document.getElementById("webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+  alpha: true,
+});
+
+renderer.setSize(sizes.width, sizes.width);
+
+function animate() {
+  requestAnimationFrame(animate);
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+animate();
